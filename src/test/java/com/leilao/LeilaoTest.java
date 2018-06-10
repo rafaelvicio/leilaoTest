2c12cf3c5aa02decf3e2cf0d924372943d6926b2
Rafael Vicio <rafaelvicio@icloud.com>
2018-06-10 01:42:14 -0300

    Testando Leilao

---
+++ src/test/java/com/leilao/LeilaoTest.java
@@ -0,0 +1,31 @@
+package com.leilao;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class LeilaoTest {
+
+    @Test
+    public void deveReceberUmLance() {
+        Leilao leilao = new Leilao("Notebook");
+        assertEquals(0, leilao.getLances().size());
+
+        leilao.propoe(new Lance(new Usuario("Steve Jobs"), 2000));
+
+        assertEquals(1, leilao.getLances().size());
+        assertEquals(2000, leilao.getLances().get(0).getValor(), 0.00001);
+    }
+
+    @Test
+    public void deveReceberVariosLances() {
+        Leilao leilao = new Leilao("xBox");
+
+        leilao.propoe(new Lance(new Usuario("Steve Jobs"), 2000));
+        leilao.propoe(new Lance(new Usuario("Steve Wozniak"), 3000));
+
+        assertEquals(2, leilao.getLances().size());
+        assertEquals(2000, leilao.getLances().get(0).getValor(), 0.00001);
+        assertEquals(3000, leilao.getLances().get(1).getValor(), 0.00001);
+    }
+}
