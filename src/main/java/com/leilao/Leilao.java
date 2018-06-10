36d0d7d12deb4da8b2f93554162396c2a80fa5a5
Rafael Vicio <rafaelvicio@icloud.com>
2018-06-10 09:46:37 -0300

    Mais testes

--- src/main/java/com/leilao/Leilao.java
+++ src/main/java/com/leilao/Leilao.java
@@ -8,14 +8,27 @@ public class Leilao {
 
 	private String descricao;
 	private List<Lance> lances;
-	
+
 	public Leilao(String descricao) {
 		this.descricao = descricao;
 		this.lances = new ArrayList<Lance>();
 	}
-	
+
 	public void propoe(Lance lance) {
-		lances.add(lance);
+
+		int total = 0;
+		for (Lance l : lances) {
+			if (l.getUsuario().equals(lance.getUsuario()))
+				total++;
+		}
+
+		if (lances.isEmpty() || (!ultimoLanceDado().getUsuario().equals(lance.getUsuario()) && total < 5)) {
+			lances.add(lance);
+		}
+	}
+
+	private Lance ultimoLanceDado() {
+		return lances.get(lances.size() - 1);
 	}
 
 	public String getDescricao() {
@@ -26,6 +39,4 @@ public class Leilao {
 		return Collections.unmodifiableList(lances);
 	}
 
-	
-	
 }
--- src/test/java/com/leilao/LeilaoTest.java
+++ src/test/java/com/leilao/LeilaoTest.java
@@ -28,4 +28,42 @@ public class LeilaoTest {
         assertEquals(2000, leilao.getLances().get(0).getValor(), 0.00001);
         assertEquals(3000, leilao.getLances().get(1).getValor(), 0.00001);
     }
+
+    @Test
+    public void naoDeveAceitarDoisLanchesSeguidosDoMesmoUsusario() {
+        Leilao leilao = new Leilao("Macbook Pro 15");
+        Usuario steveJobs = new Usuario("Steve Jobs");
+
+        leilao.propoe(new Lance(steveJobs, 2000));
+        leilao.propoe(new Lance(steveJobs, 3000));
+
+        assertEquals(1, leilao.getLances().size());
+        assertEquals(2000, leilao.getLances().get(0).getValor(), 0.00001);
+    }
+
+    @Test
+    public void naoDeveAceitarMaisDoQue5LancesDeUmMesmoUsuario() {
+        Leilao leilao = new Leilao("Macbook Pro 15");
+        Usuario steveJobs = new Usuario("Steve Jobs");
+        Usuario billGates = new Usuario("Bill Gates");
+
+        leilao.propoe(new Lance(steveJobs, 2000));
+        leilao.propoe(new Lance(billGates, 3000));
+        leilao.propoe(new Lance(steveJobs, 4000));
+        leilao.propoe(new Lance(billGates, 5000));
+        leilao.propoe(new Lance(steveJobs, 6000));
+        leilao.propoe(new Lance(billGates, 7000));
+        leilao.propoe(new Lance(steveJobs, 8000));
+        leilao.propoe(new Lance(billGates, 9000));
+        leilao.propoe(new Lance(steveJobs, 10000));
+        leilao.propoe(new Lance(billGates, 11000));
+
+        // deve ser ignorado
+        leilao.propoe(new Lance(steveJobs, 12000));
+
+        assertEquals(10, leilao.getLances().size());
+        int ultimo = leilao.getLances().size() - 1;
+        Lance ultimoLance = leilao.getLances().get(ultimo);
+        assertEquals(11000.0, ultimoLance.getValor(), 0.00001);
+    }
 }
